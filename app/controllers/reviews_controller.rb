07a645f7b757fec2baa45b0effb9dd3e126ea446
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2018-05-16 18:48:34 -0400

    yelp-mvp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+
+    redirect_to @restaurant
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:name, :address, :category)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  # Get only 5 stars restaurants
+  get('/restaurants/top', { to: "restaurants#top" })
+
+  # Get a restaurants' chef
+  get '/restaurants/:id/chef', to: "restaurants#chef"
+
+  resources :restaurants do
+    # GET /restaurants/:restaurant_id/reviews
+    # GET /restaurants/:restaurant_id/reviews/new
+    # POST /restaurants/:restaurant_id/reviews
+    resources :reviews, only: [:index, :new, :create]
+
+    # You could also do this here:
+
+    # get '/chef', to: "restaurants#chef"
+    # collection do
+    #   # get '/top', to: "restaurants#top"
+    # end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
